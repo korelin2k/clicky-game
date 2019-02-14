aa13124a641998cfdb37606e65e517ddfd7235b2
Chris Figgins <figginsc@gmail.com>
2019-02-13 22:31:45 -0500

    Updates

---
+++ precache-manifest.8f6ef3e2e4c9c1319d8819622e0317ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "65ff5c0bcd39dac5ec28",
+    "url": "/clicky-game/static/js/main.65ff5c0b.chunk.js"
+  },
+  {
+    "revision": "0d5e1e8728266cf855c0",
+    "url": "/clicky-game/static/js/2.0d5e1e87.chunk.js"
+  },
+  {
+    "revision": "65ff5c0bcd39dac5ec28",
+    "url": "/clicky-game/static/css/main.f31bdbe9.chunk.css"
+  },
+  {
+    "revision": "7ec11c110d6c8d9697d741b247a03a4e",
+    "url": "/clicky-game/index.html"
+  }
+];
